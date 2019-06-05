a517fc2e991a5f3104753078456a9bae910a54ee
Phoebe Quincy <phoebequincy@gmail.com>
2019-06-04 23:28:21 -0600

    Fixed error where add button wasn't adding.

--- src/App.js
+++ src/App.js
@@ -10,58 +10,58 @@ class App extends Component{
     this.state = {
         noteText: '',
         notes: [],
-      }
     }
+  }
 
   updateNoteText(noteText){
-    this.setState({ noteText: noteText.target.value})
+    this.setState({ noteText: noteText.target.value })
   }
 
   addNote(){
-    if(this.state.noteText === ''){
-    return false
-    }
-    let notesArr = this.state.notes;
-    notesArr.push(this.state.noteText);
-    this.setState({ noteText: ''});
+    if(this.state.noteText === ''){return}
+    let notesArr = this.state.notes.slice();
+      notesArr.push(this.state.noteText);
+    this.setState({ noteText: '', notes: notesArr});
     this.textInput.focus();
   }
 
   handleKeyPress = (event) => {
     if (event.key === 'Enter'){
-
+      let notesArr = this.state.notes;
+        notesArr.push(this.state.noteText);
+        this.setState({ notes: notesArr })
     }
   }
 
   deleteNote(index){
-    let notesArr = this.satte.notes;
+    let notesArr = this.state.notes;
     notesArr.splice(index, 1);
     this.setState({ notes: notesArr })
   }
 
   render() {
-    let notes = this.state.notes.map((val,key) => { return <Note
-            key={key}
-            text={val}
-            deleteMethod={() => this.deleteNote(key)} />
-          })
+    let notes = this.state.notes.map((val,key) => {
+      return <Note
+                key={key}
+                text={val}
+                deleteMethod={() => this.deleteNote(key)}
+             />
+    })
 
     return (
         <div className="container">
 
-          <div className = "header">To Do List</div>
-          {notes}
+          <div className = "header">To Do List</div>{notes}
 
           <div className="btn" onClick={this.addNote.bind(this)}>+</div>
 
-            <input type="text"
-              ref={((input) => {this.textInput = input})}
-              className="textInput"
-              value={this.state.noteText}
-              onChange={noteText => this.updateNoteText(noteText)}
-              onKeyPress={this.handleKeyPress.bind(this)}
-            />
-
+          <input type="text"
+            ref={((input) => {this.textInput = input})}
+            className="textInput"
+            value={this.state.noteText}
+            onChange={noteText => this.updateNoteText(noteText)}
+            onKeyPress={this.handleKeyPress.bind(this)}
+          />
         </div>
     );
   }
--- src/components/Note.js
+++ src/components/Note.js
@@ -4,9 +4,9 @@ import React, { Component } from 'react';
 class Note extends Component {
 
   render(){
-  return (
-    <div className="note" onClick={this.props.deleteMethod}>{this.props.text}
-    </div>
+    return (
+      <div className="note" onClick={this.props.deleteMethod}>{this.props.text}
+      </div>
     );
   }
 }
